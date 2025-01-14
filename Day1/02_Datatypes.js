5888b19e23349b9eb938c191f44b63bcf2901972
hemeshwari vishwakarma <43043605+hemeshwari@users.noreply.github.com>
2025-01-14 11:35:23 +0000

    datatypes

---
+++ Day1/02_Datatypes.js
@@ -0,0 +1,37 @@
+"use strict" //Treat all the code as newer version.
+
+
+// alert("Hi Hemeshwari!"); // In node js we can't use alert directly
+
+//Window.alert("Hi Hemeshwari") // will execute in browser
+
+
+/********** data types **********/
+
+// BigInt
+// String
+// Boolean
+// Number
+// null
+// undefined
+// Symbol
+
+// Object
+
+
+// typeof => To know the type of variable
+
+
+// console.log(typeof BigInt)         //function
+// console.log(typeof String)         //function
+// console.log(typeof Boolean)        //function
+// console.log(typeof Number)         //function
+// console.log(typeof null)           //Object
+// console.log(typeof undefined)      //undefined
+// console.log(typeof Symbol)         //function
+// console.log(typeof NaN)            //number
+
+
+
+
+
