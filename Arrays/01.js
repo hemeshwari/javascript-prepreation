2789e8bafa1ab83947c53330a847375fd03240b0
hemeshwari vishwakarma <43043605+hemeshwari@users.noreply.github.com>
2025-02-01 04:20:13 +0000

    arrays

---
+++ Arrays/01.js
@@ -0,0 +1,17 @@
+const array = [1,2,3,4,5,6,7,8,9,10]
+// console.log(array)
+
+const array2 = ["nisha", "heme", "himi", "anushree"]
+// console.log(array2)
+
+const array3 = new Array(0,1,2,3,4,5,6,7,8,9,10);
+// console.log(array3)
+
+console.log(array3.length);
+console.log(array3.indexOf(6))
+
+console.log(array3.includes(9))
+console.log(array3.includes(19))
+
+
+
